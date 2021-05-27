cbc07ca42689a7930366e90b4dd859355d8afd5e
Nico Mollet <nico.mollet@gmail.com>
2021-05-27 14:22:38 +0200

    Remove debugging

--- admin/js/tmsm-aquatonic-course-booking-admin.js
+++ admin/js/tmsm-aquatonic-course-booking-admin.js
@@ -25,20 +25,16 @@
    * Refresh counter
    */
   var refreshCounter = $('#refresh-counter');
-  console.log('refresh init');
   var remainingTimeMillisecond = refreshCounter.attr('data-time') * 1000; //multiply by 1000 because javascript timestamps are in ms
   var currentTime = new Date();
-  console.log('remainingTimeMillisecond:'+remainingTimeMillisecond);
   var endDate = new Date(remainingTimeMillisecond);
   var executeAfterElapsed = false;
   refreshCounter.countdown(endDate, {elapse: true}).on('update.countdown', function(event) {
-    console.log('update.countdown');
     var $this = $(this).html(event.strftime(''
       + '<span>%M</span> min '
       + '<span>%S</span> sec'));
     if (event.elapsed && executeAfterElapsed === false) {
       executeAfterElapsed = true;
-      console.log('refreshing');
       document.location.reload();
     }
   });
