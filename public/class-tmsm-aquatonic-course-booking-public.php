fe9ec156e1313bce2d3b72a82b50499e1eb067d3
jlemorvan <jlemorvan@thalasso-saintmalo.com>
2022-01-05 10:12:21 +0100

    work for > and < to 1 days

--- public/class-tmsm-aquatonic-course-booking-public.php
+++ public/class-tmsm-aquatonic-course-booking-public.php
@@ -2639,27 +2639,23 @@ class Tmsm_Aquatonic_Course_Booking_Public {
 			$difference_days   = intval( $difference->format( "%r%a" ) );
 			$difference_hours  = $difference->h;
 			$difference_hours  = $difference_hours + ( $difference->days * 24 );
-			//           var_dump($difference_hours);
-			//            var_dump($difference_days);
-			//         var_dump(intval($this->get_option( 'hoursafter' )));
 			if ( $difference_days > 1 ) {
 				$outpout = __( "Booking will be available in $difference_days days." );
-
-			} elseif ( $difference_days == 1 ) {
+			} elseif ( $difference_days === 1 ) {
 				$outpout = __( "Booking will be available tomorow" );
 			} elseif ( $difference_days < 0 ) {
 				if ( ( $difference_hours ) < intval( $this->get_option( 'hoursafter' ) ) ) {
 					$hoursleft = intval( $this->get_option( 'hoursafter' ) ) - ( $difference_hours );
 					$daysleft  = intval( floor( $hoursleft / 24 ) );
-					if ( $daysleft == 0 ) {
-						$outpout = __( "Booking is available for $hoursleft hours." );
+					if ( $daysleft === 0 ) {
+						$outpout = __( "Booking is available for $difference_hours hours." );
 					} elseif ( $daysleft > 0 ) {
 						$outpout = __( "Booking is available for $daysleft days." );
 					} else {
 						$outpout = "";
 					}
 				}
-			} elseif ( $difference_days == 0 ) {
+			} elseif ( $difference_days === 0 ) {
 				$hoursleft = $this->get_option( 'hoursafter' ) - ( $difference_hours );
 				$outpout   = __( "Booking will be available in $hoursleft hours." );
 			}
