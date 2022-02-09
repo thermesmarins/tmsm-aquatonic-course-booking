f73666832b3a083f7d0af89558551b1cab329a55
Nico Mollet <nico.mollet@gmail.com>
2022-02-09 16:34:03 +0100

    Download PDF: disable button after click

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+* Download PDF: disable button after click
+
 ### 1.9.7: February 9th, 2022
 * Download PDF: renamed filename
 * Download PDf: removed unused code
--- public/js/tmsm-aquatonic-course-booking-public.js
+++ public/js/tmsm-aquatonic-course-booking-public.js
@@ -806,7 +806,7 @@ var TmsmAquatonicCourseApp = TmsmAquatonicCourseApp || {};
 
   // Disable PDF link after click
   $('#tmsm-aquatonic-course-booking-download-pdf').on('click', function (e) {
-    //$(this).addClass('disabled');
+    $(this).addClass('disabled');
     $(this).text(TmsmAquatonicCourseApp.i18n.downloading);
   });
 
